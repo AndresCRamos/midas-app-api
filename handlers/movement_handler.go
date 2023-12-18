b5c9c635249038b0dd98c2aba2b3fa3748470fc1
AndresCRamos <57693598+AndresCRamos@users.noreply.github.com>
2023-12-18 15:18:44 -0500

    Add handler example

---
+++ handlers/movement_handler.go
@@ -0,0 +1,30 @@
+package handlers
+
+import (
+	"net/http"
+
+	"github.com/AndresCRamos/midas-app-api/services"
+	"github.com/gin-gonic/gin"
+)
+
+type movementHandler struct {
+	s services.MovementService
+}
+
+func NewMovementHandler(s services.MovementService) *movementHandler {
+	return &movementHandler{
+		s: s,
+	}
+}
+
+func (h *movementHandler) GetMovementByID(c *gin.Context) {
+	id := c.Param("id")
+
+	movement, err := h.s.GetMovementByID(id)
+
+	if err != nil {
+		c.AbortWithStatusJSON(http.StatusInternalServerError, gin.H{"message": "error"})
+	}
+
+	c.JSON(200, movement)
+}
