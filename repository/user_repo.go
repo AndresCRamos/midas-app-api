059b302bb2cce1be03e64297a1fe30c584dd5126
AndresCRamos <57693598+AndresCRamos@users.noreply.github.com>
2023-12-20 10:59:53 -0500

    User Repo implementation

---
+++ repository/user_repo.go
@@ -0,0 +1,54 @@
+package repository
+
+import (
+	"context"
+	"fmt"
+
+	"cloud.google.com/go/firestore"
+	"github.com/AndresCRamos/midas-app-api/models"
+	error_utils "github.com/AndresCRamos/midas-app-api/utils/errors"
+)
+
+type UserRepository interface {
+	GetUserByID(id string) (models.User, error)
+	CreateNewUser(user models.User) error
+}
+
+type UserRepositoryImplementation struct {
+	client *firestore.Client
+}
+
+func NewUserRepository(client *firestore.Client) *UserRepositoryImplementation {
+	return &UserRepositoryImplementation{
+		client: client,
+	}
+}
+
+func (r *UserRepositoryImplementation) GetUserByID(id string) (models.User, error) {
+	userCollection := r.client.Collection("users")
+
+	userDoc, err := userCollection.Doc(id).Get(context.Background())
+
+	if err != nil {
+		return models.User{}, error_utils.CheckFirebaseError(err, id, models.User{})
+	}
+
+	var user models.User
+
+	if err = userDoc.DataTo(user); err != nil {
+		return models.User{}, fmt.Errorf("Cant parse info for user %s", id)
+	}
+
+	return user, nil
+}
+
+func (r *UserRepositoryImplementation) CreateNewUser(user models.User) error {
+	userCollection := r.client.Collection("users")
+
+	_, err := userCollection.Doc(user.UID).Set(context.Background(), user)
+
+	if err != nil {
+		return error_utils.CheckFirebaseError(err, user.UID, user)
+	}
+	return nil
+}
