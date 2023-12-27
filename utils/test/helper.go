bfe52bec3c5abb7e9e7138c39581675660e09475
AndresCRamos <57693598+AndresCRamos@users.noreply.github.com>
2023-12-27 18:05:30 -0500

    Create GetValueByNameAndType function

--- utils/test/helper.go
+++ utils/test/helper.go
@@ -1,6 +1,7 @@
 package test
 
 import (
+	"reflect"
 	"testing"
 )
 
@@ -18,3 +19,24 @@ type PreTestFunc func(t *testing.T)
 type Fields map[string]interface{}
 
 type Args map[string]interface{}
+
+func GetValueByNameAndType(t *testing.T, args Args, name string, targetType reflect.Type) interface{} {
+	value, ok := args[name]
+	if !ok {
+		t.Fatalf("name '%s' not found in Args", name)
+	}
+
+	valueType := reflect.TypeOf(value)
+
+	if valueType.AssignableTo(targetType) {
+		return value
+	}
+
+	convertedValue := reflect.New(targetType).Elem()
+	if !convertedValue.Type().ConvertibleTo(valueType) {
+		t.Fatalf("unable to convert value for name '%s' to type '%s'", name, targetType)
+	}
+
+	convertedValue.Set(reflect.ValueOf(value).Convert(targetType))
+	return convertedValue.Interface()
+}
