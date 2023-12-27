db58142f1ca6373c94f319498da626e54e27cff6
AndresCRamos <57693598+AndresCRamos@users.noreply.github.com>
2023-12-27 15:27:30 -0500

    Created UserService mock

---
+++ utils/test/mocks/user_service.go
@@ -0,0 +1,38 @@
+package mocks
+
+import (
+	"fmt"
+
+	"github.com/AndresCRamos/midas-app-api/models"
+	error_const "github.com/AndresCRamos/midas-app-api/utils/errors"
+)
+
+type UserServiceMock struct{}
+
+func (r UserServiceMock) CreateNewUser(user models.User) error {
+	switch user.Name {
+	case "Success":
+		return nil
+	case "CantConnect":
+		return error_const.UNKNOWN
+	case "Duplicated":
+		return fmt.Errorf(error_const.ALREADY_EXISTS, user.UID)
+	default:
+		return fmt.Errorf(error_const.INVALID_TEST_CASE, user)
+	}
+}
+
+func (r UserServiceMock) GetUserByID(id string) (models.User, error) {
+	switch id {
+	case "0":
+		return TestUser, nil
+	case "1":
+		return models.User{}, error_const.UNKNOWN
+	case "2":
+		return models.User{}, fmt.Errorf(error_const.FIRESTORE_NOT_FOUND, id)
+	case "3":
+		return models.User{}, fmt.Errorf(error_const.PARSING_ERROR, id, "user")
+	default:
+		return models.User{}, fmt.Errorf(error_const.INVALID_TEST_CASE, id)
+	}
+}
