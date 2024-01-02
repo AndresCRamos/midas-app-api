b3ffac38eacde86ab85a3733d138fbed2df5db42
AndresCRamos <57693598+AndresCRamos@users.noreply.github.com>
2024-01-02 16:19:53 -0500

    Add required struct tag to UID User model

--- models/users.go
+++ models/users.go
@@ -1,7 +1,7 @@
 package models
 
 type User struct {
-	UID      string `firestore:"uid" json:"uid"`
+	UID      string `firestore:"uid" json:"uid" binding:"required"`
 	Alias    string `firestore:"alias" json:"alias"`
 	Name     string `firestore:"name" json:"name"`
 	LastName string `firestore:"last_name" json:"last_name"`
