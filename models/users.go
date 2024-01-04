9e9a9206399da8f676b4c1f1bef5cabcd056caeb
AndresCRamos <57693598+AndresCRamos@users.noreply.github.com>
2024-01-04 01:16:07 -0500

    Add User validation to include Alias or name

--- models/users.go
+++ models/users.go
@@ -2,7 +2,7 @@ package models
 
 type User struct {
 	UID      string `firestore:"uid" json:"uid" binding:"required"`
-	Alias    string `firestore:"alias" json:"alias"`
-	Name     string `firestore:"name" json:"name"`
+	Alias    string `firestore:"alias" json:"alias" binding:"required_without=Name"`
+	Name     string `firestore:"name" json:"name" binding:"required_without=Alias"`
 	LastName string `firestore:"last_name" json:"last_name"`
 }
