5b486f2718658a36584d1136c60dce64a23e86a6
AndresCRamos <57693598+AndresCRamos@users.noreply.github.com>
2024-01-07 14:33:46 -0500

    index on users-endpoint: 64718b0 Change handler test to use Gin in Release mode

--- models/users.go
+++ models/users.go
@@ -1,8 +1,17 @@
 package models
 
+import "github.com/go-playground/validator/v10"
+
 type User struct {
 	UID      string `firestore:"uid" json:"uid" binding:"required"`
 	Alias    string `firestore:"alias" json:"alias" binding:"required_without=Name"`
 	Name     string `firestore:"name" json:"name" binding:"required_without=Alias"`
-	LastName string `firestore:"last_name" json:"last_name"`
+	LastName string `firestore:"last_name" json:"last_name" binding:"required_with=Name"`
+}
+
+func (u *User) ValidateStructLevel(sl validator.StructLevel) {
+	// Check if last_name is present only if name is present
+	if u.Name == "" && u.LastName != "" {
+		sl.ReportError(u.LastName, "LastName", "last_name", "dependsOn", "")
+	}
 }
