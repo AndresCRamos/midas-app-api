940aa96617242116b544387fbd302ee9a2477572
AndresCRamos <57693598+AndresCRamos@users.noreply.github.com>
2024-01-09 17:27:45 -0500

    Add middleware to verify User Token

--- routes/user_routes.go
+++ routes/user_routes.go
@@ -1,19 +1,24 @@
 package routes
 
 import (
-	"cloud.google.com/go/firestore"
+	"github.com/AndresCRamos/midas-app-api/cmd/server"
 	"github.com/AndresCRamos/midas-app-api/handlers"
+	"github.com/AndresCRamos/midas-app-api/middleware"
 	"github.com/AndresCRamos/midas-app-api/repository"
 	"github.com/AndresCRamos/midas-app-api/services"
-	"github.com/gin-gonic/gin"
 )
 
-func addUserRoutes(client *firestore.Client, r *gin.Engine) {
+func addUserRoutes(server *server.Server) {
+
+	client := server.FirestoreClient
+	r := server.Router
+
 	repo := repository.NewUserRepository(client)
 	service := services.NewUserService(repo)
 	handler := handlers.NewUserHandler(service)
 
 	userGroup := r.Group("/user")
+	userGroup.Use(middleware.VerifyToken(server.FirebaseAuthClient))
 	{
 		userGroup.POST("/", handler.CreateNewUser)
 		userGroup.GET("/:id", handler.GetUserByID)
