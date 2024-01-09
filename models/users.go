a3bd83a2323660bb08ab680d846d5666a72954e4
AndresCRamos <57693598+AndresCRamos@users.noreply.github.com>
2024-01-08 23:02:12 -0500

    Add depends_on validation to User last name field

--- models/users.go
+++ models/users.go
@@ -4,5 +4,5 @@ type User struct {
 	UID      string `firestore:"uid" json:"uid" binding:"required"`
 	Alias    string `firestore:"alias" json:"alias" binding:"required_without=Name"`
 	Name     string `firestore:"name" json:"name" binding:"required_without=Alias"`
-	LastName string `firestore:"last_name" json:"last_name"`
+	LastName string `firestore:"last_name" json:"last_name" binding:"depends_on=Name"`
 }
