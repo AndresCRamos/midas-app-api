eebe5614b42dd9ab2e0a89cea71cc823e1fc1d7a
AndresCRamos <57693598+AndresCRamos@users.noreply.github.com>
2024-01-10 14:01:47 -0500

    Created basic implementations for CreateSource and GetSourcebyID

---
+++ repository/source_repo.go
@@ -0,0 +1,58 @@
+package repository
+
+import (
+	"context"
+
+	"cloud.google.com/go/firestore"
+	"github.com/AndresCRamos/midas-app-api/models"
+	error_utils "github.com/AndresCRamos/midas-app-api/utils/errors"
+)
+
+type SourceRepository interface {
+	GetSourceByID(id string) (models.Source, error)
+	CreateNewSource(Source models.Source) error
+}
+
+type SourceRepositoryImplementation struct {
+	client *firestore.Client
+}
+
+func NewSourceRepository(client *firestore.Client) *SourceRepositoryImplementation {
+	return &SourceRepositoryImplementation{
+		client: client,
+	}
+}
+
+func (r *SourceRepositoryImplementation) GetSourceByID(id string) (models.Source, error) {
+	SourceCollection := r.client.Collection("sources")
+
+	SourceDoc, err := SourceCollection.Doc(id).Get(context.Background())
+
+	if err != nil {
+		wrapEr := error_utils.SourceRepositoryError{}
+		return models.Source{}, error_utils.CheckFirebaseError(err, id, &wrapEr)
+	}
+
+	var Source models.Source
+
+	if err = SourceDoc.DataTo(&Source); err != nil {
+		wrapErr := error_utils.SourceRepositoryError{}
+		logged_err := error_utils.FirestoreParsingError{DocID: Source.UID, StructName: "Source"}
+		wrapErr.Wrap(logged_err)
+		return models.Source{}, wrapErr
+	}
+
+	return Source, nil
+}
+
+func (r *SourceRepositoryImplementation) CreateNewSource(Source models.Source) error {
+	SourceCollection := r.client.Collection("sources")
+
+	_, err := SourceCollection.Doc(Source.UID).Create(context.Background(), Source)
+
+	if err != nil {
+		wrapErr := error_utils.SourceRepositoryError{}
+		return error_utils.CheckFirebaseError(err, Source.UID, &wrapErr)
+	}
+	return nil
+}
