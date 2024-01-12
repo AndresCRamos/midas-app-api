3a7c1756172714474b70961650db668205efe881
AndresCRamos <57693598+AndresCRamos@users.noreply.github.com>
2024-01-12 15:22:02 -0500

    Simplified SourceRepo mock

--- utils/test/mocks/source_repo.go
+++ utils/test/mocks/source_repo.go
@@ -7,19 +7,19 @@ import (
 
 type SourceRepositoryMock struct{}
 
-func (r SourceRepositoryMock) CreateNewSource(user models.Source) error {
+func (r SourceRepositoryMock) CreateNewSource(source models.Source) error {
 	wrapper := error_const.SourceRepositoryError{}
-	switch user.Name {
+	switch source.Name {
 	case "Success":
 		return nil
 	case "CantConnect":
 		wrapper.Wrap(error_const.FirebaseUnknownError{})
 		return wrapper
 	case "Duplicated":
-		wrapper.Wrap(error_const.FirestoreAlreadyExistsError{DocID: user.UID})
+		wrapper.Wrap(error_const.FirestoreAlreadyExistsError{DocID: source.UID})
 		return wrapper
 	default:
-		return error_const.TestInvalidTestCaseError{Param: user.Name}
+		return error_const.TestInvalidTestCaseError{Param: source.Name}
 	}
 }
 
@@ -44,40 +44,41 @@ func (r SourceRepositoryMock) GetSourceByID(id string) (models.Source, error) {
 	}
 }
 
-func (r SourceRepositoryMock) UpdateSource(id string) (models.Source, error) {
+func (r SourceRepositoryMock) UpdateSource(source models.Source) error {
+	id := source.UID
 	wrapper := error_const.SourceRepositoryError{}
 	switch id {
 	case "0":
-		return TestSource, nil
+		return nil
 	case "1":
 		wrapper.Wrap(error_const.FirebaseUnknownError{})
-		return models.Source{}, wrapper
+		return wrapper
 	case "2":
 		wrapper.Wrap(error_const.FirestoreNotFoundError{DocID: id})
-		return models.Source{}, wrapper
+		return wrapper
 	case "3":
 		wrapper.Wrap(error_const.FirestoreParsingError{DocID: id, StructName: "user"})
-		return models.Source{}, wrapper
+		return wrapper
 	default:
-		return models.Source{}, error_const.TestInvalidTestCaseError{Param: id}
+		return error_const.TestInvalidTestCaseError{Param: id}
 	}
 }
 
-func (r SourceRepositoryMock) DeleteSource(id string) (models.Source, error) {
+func (r SourceRepositoryMock) DeleteSource(id string) error {
 	wrapper := error_const.SourceRepositoryError{}
 	switch id {
 	case "0":
-		return TestSource, nil
+		return nil
 	case "1":
 		wrapper.Wrap(error_const.FirebaseUnknownError{})
-		return models.Source{}, wrapper
+		return wrapper
 	case "2":
 		wrapper.Wrap(error_const.FirestoreNotFoundError{DocID: id})
-		return models.Source{}, wrapper
+		return wrapper
 	case "3":
 		wrapper.Wrap(error_const.FirestoreParsingError{DocID: id, StructName: "user"})
-		return models.Source{}, wrapper
+		return wrapper
 	default:
-		return models.Source{}, error_const.TestInvalidTestCaseError{Param: id}
+		return error_const.TestInvalidTestCaseError{Param: id}
 	}
 }
