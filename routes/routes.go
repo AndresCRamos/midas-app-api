a859de677f6beacaa25e87a46a5be034cd05ca7f
AndresCRamos <57693598+AndresCRamos@users.noreply.github.com>
2024-01-12 21:12:50 -0500

    Add Source routes

--- routes/routes.go
+++ routes/routes.go
@@ -5,4 +5,5 @@ import "github.com/AndresCRamos/midas-app-api/cmd/server"
 func AddRoutes(server *server.Server) {
 	addMovementRoutes(server.FirestoreClient, server.Router)
 	addUserRoutes(server)
+	addSourceRoutes(server)
 }
