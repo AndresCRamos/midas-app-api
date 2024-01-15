9454737215544f6ae28288f4aecc9767d0a4d838
AndresCRamos <57693598+AndresCRamos@users.noreply.github.com>
2024-01-15 13:12:00 -0500

    Export CreatedAt and UpdatedAt fields in Source model

--- models/sources.go
+++ models/sources.go
@@ -3,18 +3,18 @@ package models
 import "time"
 
 type Source struct {
-	UID         string `json:"uid" firestore:"uid" binding:"required"`
-	Name        string `json:"name" firestore:"name" binding:"required"`
-	OwnerId     string `json:"ownerId" firestore:"owner" binding:"required"`
-	Description string `json:"description,omitempty" firestore:"description"`
-	createdAt   time.Time
-	updatedAt   time.Time
+	UID         string    `json:"uid" firestore:"uid" binding:"required"`
+	Name        string    `json:"name" firestore:"name" binding:"required"`
+	OwnerId     string    `json:"ownerId" firestore:"owner" binding:"required"`
+	Description string    `json:"description,omitempty" firestore:"description"`
+	CreatedAt   time.Time `json:"-" firestore:"created_at"`
+	UpdatedAt   time.Time `json:"-" firestore:"updated_at"`
 }
 
 func (s *Source) NewCreationAtDate() {
-	s.createdAt = time.Now()
+	s.CreatedAt = time.Now()
 }
 
 func (s *Source) NewUpdatedAtDate() {
-	s.updatedAt = time.Now()
+	s.UpdatedAt = time.Now()
 }
