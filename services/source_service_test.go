1d0ae17220a3c369807c5f5f79a599a2ec5477b6
AndresCRamos <57693598+AndresCRamos@users.noreply.github.com>
2024-02-02 17:26:54 -0500

    Add tests for Source.Service GetSourcesByUser

--- services/source_service_test.go
+++ services/source_service_test.go
@@ -5,6 +5,7 @@ import (
 
 	"github.com/AndresCRamos/midas-app-api/models"
 	"github.com/AndresCRamos/midas-app-api/repository"
+	util_models "github.com/AndresCRamos/midas-app-api/utils/api/models"
 	error_utils "github.com/AndresCRamos/midas-app-api/utils/errors"
 	test_utils "github.com/AndresCRamos/midas-app-api/utils/test"
 	"github.com/AndresCRamos/midas-app-api/utils/test/mocks"
@@ -65,6 +66,79 @@ func Test_sourceServiceImplementation_CreateNewSource(t *testing.T) {
 			_, err := s.CreateNewSource(*sourceTest)
 			if !tt.WantErr {
 				assert.NoError(t, err)
+			} else {
+				assert.Error(t, err)
+				assert.ErrorAs(t, err, &tt.ExpectedErr)
+			}
+		})
+
+	}
+}
+
+func Test_sourceServiceImplementation_GetSourcesByUser(t *testing.T) {
+
+	mockRepo := mocks.SourceRepositoryMock{}
+
+	fields := test_utils.Fields{
+		"mockRepo": mockRepo,
+	}
+
+	tests := []test_utils.TestCase{
+		{
+			Name:   "Success",
+			Fields: fields,
+			Args: test_utils.Args{
+				"userID": "0",
+				"expectedData": util_models.PaginatedSearch[models.Source]{
+					CurrentPage: 1,
+					TotalData:   1,
+					PageSize:    1,
+					Data: []models.Source{
+						mocks.TestSource,
+					},
+				},
+			},
+			WantErr:     false,
+			ExpectedErr: nil,
+			PreTest:     nil,
+		},
+		{
+			Name:   "Fail to connect",
+			Fields: fields,
+			Args: test_utils.Args{
+				"userID": "1",
+			},
+			WantErr:     true,
+			ExpectedErr: error_utils.FirebaseUnknownError{},
+			PreTest:     nil,
+		},
+		{
+			Name:   "Not enough data",
+			Fields: fields,
+			Args: test_utils.Args{
+				"userID": "2",
+			},
+			WantErr:     true,
+			ExpectedErr: error_utils.SourceNotEnoughData{},
+			PreTest:     nil,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.Name, func(t *testing.T) {
+			if tt.PreTest != nil {
+				tt.PreTest(t)
+			}
+			mockRepo := test_utils.GetFieldByNameAndType(t, tt.Fields, "mockRepo", new(repository.SourceRepository))
+			s := &sourceServiceImplementation{
+				r: mockRepo.(repository.SourceRepository),
+			}
+			userId := test_utils.GetArgByNameAndType(t, tt.Args, "userID", "").(string)
+			got, err := s.GetSourcesByUser(userId, 1)
+			if !tt.WantErr {
+				expected := test_utils.GetArgByNameAndType(t, tt.Args, "expectedData", util_models.PaginatedSearch[models.Source]{})
+				assert.NoError(t, err)
+				assert.Equal(t, expected, got)
 			} else {
 				assert.Error(t, err)
 				assert.ErrorAs(t, err, &tt.ExpectedErr)
