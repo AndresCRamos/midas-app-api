f1ff0746b36e75a7a4974ec80f8649a5af96760c
AndresCRamos <57693598+AndresCRamos@users.noreply.github.com>
2024-02-13 12:48:02 -0500

    Add UserCreateModel

--- models/users.go
+++ models/users.go
@@ -1,8 +1,22 @@
 package models
 
 type User struct {
-	UID      string `firestore:"uid" json:"uid" binding:"required"`
+	UID      string `firestore:"uid" json:"uid"`
+	Alias    string `firestore:"alias" json:"alias"`
+	Name     string `firestore:"name" json:"name"`
+	LastName string `firestore:"last_name" json:"last_name"`
+}
+
+type UserCreate struct {
 	Alias    string `firestore:"alias" json:"alias" binding:"required_without=Name"`
 	Name     string `firestore:"name" json:"name" binding:"required_without=Alias"`
 	LastName string `firestore:"last_name" json:"last_name" binding:"depends_on=Name"`
+}
+
+func (uc *UserCreate) ParseUser() User {
+	return User{
+		Alias:    uc.Alias,
+		Name:     uc.Name,
+		LastName: uc.LastName,
+	}
 }
