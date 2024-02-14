26c9a45b45710a4f3db1612fc394020b6c61ad16
AndresCRamos <57693598+AndresCRamos@users.noreply.github.com>
2024-02-14 13:41:12 -0500

    Add firebase tags to movement struct

--- models/movement.go
+++ models/movement.go
@@ -5,13 +5,21 @@ import (
 )
 
 type Movement struct {
-	UID          string
-	OwnerId      string
-	Name         string
-	Description  string
-	Amount       int64
-	MovementDate time.Time
-	Tags         []string
-	CreatedAt    time.Time
-	UpdatedAt    time.Time
+	UID          string    `firebase:"uid"`
+	OwnerId      string    `firebase:"owner"`
+	Name         string    `firebase:"name"`
+	Description  string    `firebase:"description"`
+	Amount       int64     `firebase:"amount"`
+	MovementDate time.Time `firebase:"movement_date"`
+	Tags         []string  `firebase:"tags"`
+	CreatedAt    time.Time `firebase:"created_at"`
+	UpdatedAt    time.Time `firebase:"updated_at"`
+}
+
+func (m *Movement) NewCreationAtDate() {
+	m.CreatedAt = time.Now()
+}
+
+func (m *Movement) NewUpdatedAtDate() {
+	m.UpdatedAt = time.Now()
 }
