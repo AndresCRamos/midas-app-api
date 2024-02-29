1105c9d822289b089ca56985f6dfda57d1d34456
AndresCRamos <57693598+AndresCRamos@users.noreply.github.com>
2024-02-28 20:52:45 -0500

    Add GetMovementsByUserAndDate to movementHandler

--- handlers/movement_handler.go
+++ handlers/movement_handler.go
@@ -2,9 +2,12 @@ package handlers
 
 import (
 	"net/http"
+	"strconv"
+	"time"
 
 	"github.com/AndresCRamos/midas-app-api/models"
 	"github.com/AndresCRamos/midas-app-api/services"
+	util_models "github.com/AndresCRamos/midas-app-api/utils/api/models"
 	error_utils "github.com/AndresCRamos/midas-app-api/utils/errors"
 	"github.com/gin-gonic/gin"
 )
@@ -39,4 +42,66 @@ func (h *movementHandler) GetMovementByID(c *gin.Context) {
 	}
 
 	c.JSON(http.StatusOK, movementData)
+}
+
+func (h *movementHandler) GetMovementsByUserAndDate(c *gin.Context) {
+	userID, exists := c.Get("user")
+	if !exists {
+		c.AbortWithStatusJSON(error_utils.CantGetUser{}.GetAPIError())
+		return
+	}
+
+	var page int
+	var err error
+
+	pageStr, exists := c.GetQuery("page")
+	if !exists {
+		page = 1
+	} else if page, err = strconv.Atoi(pageStr); err != nil {
+		c.AbortWithStatusJSON(util_models.PaginatedTypeError{}.GetAPIError())
+		return
+	}
+
+	dateFromStr, exists := c.GetQuery("date_from")
+	var dateFrom time.Time
+	if !exists {
+		dateFrom = time.Now().UTC().Add(-30 * 24 * time.Hour)
+	} else if dateFrom, err = time.Parse(time.DateOnly, dateFromStr); err != nil {
+		c.AbortWithStatusJSON(error_utils.SourceBadDateFormat{DateString: dateFromStr, DateField: "date_from", Format: time.DateOnly}.GetAPIError())
+		return
+	}
+
+	dateToStr, exists := c.GetQuery("date_to")
+	var dateTo time.Time
+	if !exists {
+		dateTo = time.Now().UTC()
+	} else if dateTo, err = time.Parse(time.DateOnly, dateToStr); err != nil {
+		c.AbortWithStatusJSON(error_utils.SourceBadDateFormat{DateString: dateToStr, DateField: "date_to", Format: time.DateOnly}.GetAPIError())
+		return
+	}
+
+	movementSearchResult, err := h.s.GetMovementsByUserAndDate(userID.(string), page, dateFrom, dateTo)
+
+	if err != nil {
+		apiErr := error_utils.CheckServiceErrors(userID.(string), err, "movement")
+		c.AbortWithStatusJSON(apiErr.GetAPIError())
+		return
+	}
+
+	movementRetrievedData := []models.MovementRetrieve{}
+
+	for _, movementData := range movementSearchResult.Data {
+		retrievedData := models.MovementRetrieve{}
+		retrievedData.ParseMovement(movementData)
+		movementRetrievedData = append(movementRetrievedData, retrievedData)
+	}
+
+	movementSearchRetrieve := util_models.PaginatedSearch[models.MovementRetrieve]{
+		CurrentPage: movementSearchResult.CurrentPage,
+		TotalData:   movementSearchResult.TotalData,
+		PageSize:    movementSearchResult.TotalData,
+		Data:        movementRetrievedData,
+	}
+
+	c.JSON(http.StatusOK, movementSearchRetrieve)
 }
