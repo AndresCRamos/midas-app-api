bd41775d9cffb0587837d7121b2ca2c97c64f520
AndresCRamos <57693598+AndresCRamos@users.noreply.github.com>
2024-03-03 23:06:39 -0500

    Add basic tests for CreateNewMovement

--- handlers/movement_handler_test.go
+++ handlers/movement_handler_test.go
@@ -1,9 +1,14 @@
 package handlers
 
 import (
+	"bytes"
 	"encoding/json"
+	"errors"
 	"net/http"
+	"slices"
+	"strings"
 	"testing"
+	"time"
 
 	"github.com/AndresCRamos/midas-app-api/models"
 	"github.com/AndresCRamos/midas-app-api/services"
@@ -15,6 +20,12 @@ import (
 	"github.com/AndresCRamos/midas-app-api/utils/test/mocks"
 	"github.com/gin-gonic/gin"
 	"github.com/stretchr/testify/assert"
+)
+
+var (
+	movementValidationTests = []string{
+		"No Name",
+	}
 )
 
 func Test_movementHandler_GetMovementByID(t *testing.T) {
@@ -247,5 +258,132 @@ func Test_movementHandler_GetMovementsByUserAndDate(t *testing.T) {
 				assert.Equal(t, tt.ExpectedErr.Error(), errMessage["error"])
 			}
 		})
+	}
+}
+
+func Test_movementHandler_CreateNewMovement(t *testing.T) {
+	mockService := mocks.MovementServiceMock{}
+
+	fields := test_utils.Fields{
+		"mockService": mockService,
+	}
+
+	tests := []test_utils.TestCase{
+		{
+			Name:   "Success",
+			Fields: fields,
+			Args: test_utils.Args{
+				"movement":     models.MovementCreate{Name: "Success", Amount: 100, SourceID: "123", MovementDate: time.Date(2024, time.January, 25, 0, 0, 0, 0, time.UTC)},
+				"expectedCode": http.StatusCreated,
+			},
+			WantErr:     false,
+			ExpectedErr: nil,
+			PreTest:     nil,
+		},
+		{
+			Name:   "Fail to connect",
+			Fields: fields,
+			Args: test_utils.Args{
+				"movement":     models.MovementCreate{Name: "CantConnect", Amount: 100, SourceID: "123", MovementDate: time.Date(2024, time.January, 25, 0, 0, 0, 0, time.UTC)},
+				"expectedCode": http.StatusInternalServerError,
+			},
+			WantErr:     true,
+			ExpectedErr: error_utils.APIUnknown{},
+			PreTest:     nil,
+		},
+		{
+			Name:   "No Name",
+			Fields: fields,
+			Args: test_utils.Args{
+				"movement":          models.MovementCreate{Amount: 100, SourceID: "123", MovementDate: time.Date(2024, time.January, 25, 0, 0, 0, 0, time.UTC)},
+				"expectedCode":      http.StatusBadRequest,
+				"expectedErrDetail": []string{"field name is required"},
+			},
+			WantErr:     true,
+			ExpectedErr: error_utils.APIInvalidRequestBody{},
+			PreTest:     nil,
+		},
+		{
+			Name:   "Cant find source",
+			Fields: fields,
+			Args: test_utils.Args{
+				"movement":          models.MovementCreate{Name: "NoSource", Amount: 100, SourceID: "123", MovementDate: time.Date(2024, time.January, 25, 0, 0, 0, 0, time.UTC)},
+				"expectedCode":      http.StatusNotFound,
+				"expectedErrDetail": []string{"The movement  cant be created, because source 123 doesn't exists"},
+			},
+			WantErr:     true,
+			ExpectedErr: errors.New("The movement  cant be created, because source 123 doesn't exists"),
+			PreTest:     nil,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.Name, func(t *testing.T) {
+			if tt.PreTest != nil {
+				tt.PreTest(t)
+			}
+			mockService := test_utils.GetFieldByNameAndType[services.MovementService](t, tt.Fields, "mockService")
+			h := &movementHandler{
+				s: mockService,
+			}
+
+			body := getMovementTestBody[models.MovementCreate](t, tt)
+
+			testRequest := test.TestRequest{
+				Method:      http.MethodPost,
+				BasePath:    "/",
+				Handler:     h.CreateNewMovement,
+				Middlewares: []gin.HandlerFunc{test_middleware.TestMiddleware("123")},
+				Body:        bytes.NewBuffer(body),
+			}
+
+			w := testRequest.ServeRequest(t)
+
+			expectedCode := test_utils.GetArgByNameAndType[int](t, tt.Args, "expectedCode")
+			assert.Equal(t, expectedCode, w.Code)
+			if !tt.WantErr {
+
+			} else {
+				var errMessage map[string]interface{}
+				err := json.Unmarshal(w.Body.Bytes(), &errMessage)
+				assert.NoError(t, err)
+				assert.Equal(t, tt.ExpectedErr.Error(), errMessage["error"])
+
+				if slices.Contains(movementValidationTests, tt.Name) {
+					expectedDetail := test_utils.GetArgByNameAndType[[]string](t, tt.Args, "expectedErrDetail")
+					val, ok := errMessage["detail"]
+					if ok {
+						assert.Equal(t, expectedDetail[0], val.(string))
+						return
+					}
+					val, ok = errMessage["details"]
+					if ok {
+						assert.ElementsMatch(t, expectedDetail, val)
+						return
+					}
+
+					errMsjByte, _ := json.MarshalIndent(errMessage, "", " ")
+
+					t.Fatalf("Cant get error details from error message:\n %s", string(errMsjByte))
+				}
+
+			}
+		})
+	}
+}
+
+func getMovementTestBody[T any](test *testing.T, testCase test_utils.TestCase) []byte {
+	testName := strings.Split(test.Name(), "/")[1]
+
+	switch testName {
+	case "Bad_request":
+		body, _ := json.Marshal(map[string]any{
+			"InvalidBody": "Invalid",
+		})
+		return body
+	default:
+		bodyStruct := test_utils.GetArgByNameAndType[T](test, testCase.Args, "movement")
+		body, _ := json.Marshal(bodyStruct)
+		return body
 	}
 }
