166fe1f69248233f45830a5d66f00e61c94abb7d
AndresCRamos <57693598+AndresCRamos@users.noreply.github.com>
2024-03-08 23:43:54 -0500

    Defined basic e2e test structure

--- test/e2e/user_test.go
+++ test/e2e/user_test.go
@@ -1,12 +1,15 @@
 package e2e
 
 import (
+	"net/http"
 	"testing"
 
 	"github.com/AndresCRamos/midas-app-api/handlers"
 	"github.com/AndresCRamos/midas-app-api/repository"
 	"github.com/AndresCRamos/midas-app-api/services"
 	"github.com/AndresCRamos/midas-app-api/utils/firebase"
+	test_utils "github.com/AndresCRamos/midas-app-api/utils/test"
+	"github.com/stretchr/testify/assert"
 )
 
 func initUserTest(t *testing.T) *handlers.UserHandler {
@@ -24,6 +27,39 @@ func initUserTest(t *testing.T) *handlers.UserHandler {
 
 func Test_user_CreateNewUser(t *testing.T) {
 
+	userHandler := initUserTest(t)
+
+	field := test_utils.Fields{
+		"userHandler": userHandler,
+	}
+
+	tests := []test_utils.TestCase{
+		{
+			Name:    "",
+			Args:    test_utils.Args{},
+			Fields:  field,
+			WantErr: false,
+			PreTest: nil,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.Name, func(t *testing.T) {
+			handler := test_utils.GetArgByNameAndType[handlers.UserHandler](t, tt.Args, "userHandler")
+
+			testRequest := test_utils.TestRequest{
+				Method:  http.MethodPost,
+				Handler: handler.CreateNewUser,
+			}
+
+			w := testRequest.ServeRequest(t)
+
+			expectedCode := test_utils.GetArgByNameAndType[int](t, tt.Args, "expectedCode")
+
+			assert.Equal(t, expectedCode, w.Code)
+
+		})
+	}
 }
 
 func Test_user_GetUserByID(t *testing.T) {
