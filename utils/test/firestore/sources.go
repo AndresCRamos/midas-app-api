b88c0020435c25cd83e8b1f6dbf75b684f2529c3
AndresCRamos <57693598+AndresCRamos@users.noreply.github.com>
2024-03-13 22:33:56 -0500

    Add CreateTestSource helper function

--- utils/test/firestore/sources.go
+++ utils/test/firestore/sources.go
@@ -1,8 +1,11 @@
 package firestore
 
 import (
+	"context"
+	"testing"
 	"time"
 
+	"cloud.google.com/go/firestore"
 	"github.com/AndresCRamos/midas-app-api/models"
 )
 
@@ -19,9 +22,16 @@ var (
 		UpdatedAt:   time.Now(),
 	}
 
+	TestSourceRetrieveUpdated = models.SourceRetrieve{
+		Name:        "TEST_SOURCE",
+		Description: "TEST DESCRIPTION UPDATED",
+		CreatedAt:   time.Now(),
+		UpdatedAt:   time.Now(),
+	}
+
 	TestSourceUpdate = models.SourceUpdate{
 		Name:        "TEST_SOURCE",
-		Description: "TEST DESCRIPTION",
+		Description: "TEST DESCRIPTION UPDATED",
 	}
 
 	TestSource = models.Source{
@@ -31,3 +41,15 @@ var (
 		UpdatedAt:   time.Now(),
 	}
 )
+
+func CreateTestSource(t *testing.T, client *firestore.Client, ownerID string) models.Source {
+	sourceDocRef := client.Collection("sources").NewDoc()
+	tUser := TestSource
+	tUser.UID = sourceDocRef.ID
+	tUser.OwnerId = ownerID
+	_, err := sourceDocRef.Set(context.Background(), tUser)
+	if err != nil {
+		t.Fatalf("Can't create test source: %s", err)
+	}
+	return tUser
+}
