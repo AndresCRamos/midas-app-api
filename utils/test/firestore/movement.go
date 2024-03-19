b8438a93f1f9d3421887efca5a36616130acfb15
AndresCRamos <57693598+AndresCRamos@users.noreply.github.com>
2024-03-19 12:13:56 -0500

    Add CreateTestMovement function

--- utils/test/firestore/movement.go
+++ utils/test/firestore/movement.go
@@ -1,8 +1,11 @@
 package firestore
 
 import (
+	"context"
+	"testing"
 	"time"
 
+	"cloud.google.com/go/firestore"
 	"github.com/AndresCRamos/midas-app-api/models"
 )
 
@@ -45,3 +48,21 @@ var (
 		UpdatedAt:   time.Now(),
 	}
 )
+
+func SetTestMovementData(movement models.Movement, uid string, ownerID string) models.Movement {
+	movement.UID = uid
+	movement.OwnerId = ownerID
+	return movement
+}
+
+func CreateTestMovement(t *testing.T, client *firestore.Client, ownerID string) models.Movement {
+	movementDocRef := client.Collection("movements").NewDoc()
+	tUser := TestMovement
+	tUser.UID = movementDocRef.ID
+	tUser.OwnerId = ownerID
+	_, err := movementDocRef.Set(context.Background(), tUser)
+	if err != nil {
+		t.Fatalf("Can't create test movement: %s", err)
+	}
+	return tUser
+}
