2d883a1d7b8d4528c83d1bcea36b4692f2567027
Andres Camilo Ramos <57693598+AndresCRamos@users.noreply.github.com>
2024-03-20 13:16:33 -0500

    Revert "Firestore test utils"

--- routes/routes.go
+++
@@ -1,9 +0,0 @@
-package routes
-
-import "github.com/AndresCRamos/midas-app-api/cmd/server"
-
-func AddRoutes(server *server.Server) {
-	addMovementRoutes(server.FirestoreClient, server.Router)
-	addUserRoutes(server)
-	addSourceRoutes(server)
-}
